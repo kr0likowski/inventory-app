5480f2f3a9065611037d0a7cae017719cefc7a0c
Mac OS <macos@MacBook-Air-krolik.local>
2017-11-08 17:15:49 +0100

    Simple db

--- app/src/main/java/com/example/macos/inventory_app/data/ItemdbHelper.java
+++ app/src/main/java/com/example/macos/inventory_app/data/ItemdbHelper.java
@@ -1,20 +1,36 @@
 package com.example.macos.inventory_app.data;
 
+import com.example.macos.inventory_app.data.ItemContract.ItemEntry;
+
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 
 /**
  * Created by macos on 08.11.2017.
  */
 
-public class ItemdbHelper {
-   /* @Override
+public class ItemdbHelper extends SQLiteOpenHelper {
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "ItemReader.db";
+
+    public ItemdbHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        String SQL_CREATE_PETS_TABLE = "CREATE TABLE " + PetEntry.TABLE_NAME + " ("
-                + PetEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + PetEntry.COLUMN_PET_NAME + " TEXT NOT NULL, "
-                + PetEntry.COLUMN_PET_BREED + " TEXT, "
-                + PetEntry.COLUMN_PET_GENDER + " INTEGER NOT NULL, "
-                + PetEntry.COLUMN_PET_WEIGHT + " INTEGER NOT NULL DEFAULT 0);";
+        String SQL_CREATE_PETS_TABLE = "CREATE TABLE " + ItemEntry.TABLE_NAME + " ("
+                + ItemEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + ItemEntry.COLUMN_NAME_NAME + " TEXT NOT NULL, "
+                + ItemEntry.COLUMN_NAME_PRICE + " TEXT, "
+                + ItemEntry.COLUMN_NAME_QUANTITY + " INTEGER NOT NULL, "
+                + " );";
         sqLiteDatabase.execSQL(SQL_CREATE_PETS_TABLE);
-    }*/
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
